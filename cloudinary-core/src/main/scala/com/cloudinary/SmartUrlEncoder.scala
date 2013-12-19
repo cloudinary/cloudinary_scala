1c2030800eb7601dd2b89ad39771f2c8150eec6b
itaibenari <itai.benari@gmail.com>
2013-12-19 20:48:37 +0200

    Fix implementation of SmartUrlEncoder in case of non-ascii characters

--- cloudinary-core/src/main/scala/com/cloudinary/SmartUrlEncoder.scala
+++ cloudinary-core/src/main/scala/com/cloudinary/SmartUrlEncoder.scala
@@ -1,29 +1,13 @@
 package com.cloudinary
 
-// See http://stackoverflow.com/a/4605816
+import java.net.URLEncoder
+import java.io.UnsupportedEncodingException
+
 object SmartUrlEncoder {
-  val unsafe = " %$&+,;=?@<>#%".toSet
-  val hex = "0123456789ABCDEF"
-
   def encode(input: String) =
-    input flatMap {
-      ch =>
-        if (isUnsafe(ch)) {
-          List('%', hex(ch / 16), hex(ch % 16))
-        } else {
-          List(ch)
-        }
+    try {
+      URLEncoder.encode(input, "UTF-8").replace("%2F", "/").replace("%3A", ":").replace("+", "%20");
+    } catch {
+      case e: UnsupportedEncodingException => throw new RuntimeException(e);
     }
-
-  private def toHex(ch: Int) =
-    (if (ch < 10) {
-      '0' + ch.toString
-    } else {
-      'A' + (ch - 10).toString
-    })
-
-  private def isUnsafe(ch: Char): Boolean =
-    if (ch > 128 || ch < 0) {
-      true
-    } else unsafe.contains(ch)
 }
