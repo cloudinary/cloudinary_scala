138aa5bf14bfccc28c0cedcec2606bf8ca17478f
Itai Benari <itai.benari@gmail.com>
2015-01-24 22:01:22 +0200

    enable configuration with CLOUDINARY_URL in the play plugin

--- cloudinary-play-plugin/app/cloudinary/core/CloudinaryPlugin.scala
+++ cloudinary-play-plugin/app/cloudinary/core/CloudinaryPlugin.scala
@@ -11,17 +11,23 @@ import java.io.File
 
 
 class CloudinaryPlugin(app: play.api.Application) extends Plugin { 
-  lazy val cloudinary = new Cloudinary(Map(
-      "api_key" -> app.configuration.getString("cloudinary.api_key").getOrElse(throw new IllegalArgumentException("Configuration must include api_key")),
-      "api_secret" -> app.configuration.getString("cloudinary.api_secret").getOrElse(throw new IllegalArgumentException("Configuration must include api_secret")),
-      "cloud_name" -> app.configuration.getString("cloudinary.cloud_name").getOrElse(throw new IllegalArgumentException("Configuration must include cloud_name")),
-      "cname" -> app.configuration.getString("cloudinary.cname"),
-      "secure_distribution" -> app.configuration.getString("cloudinary.secure_distribution"),
-      "private_cdn" -> app.configuration.getBoolean("cloudinary.private_cdn").getOrElse(false),
-      "secure" -> app.configuration.getBoolean("cloudinary.secure").getOrElse(false),
-      "cdn_subdomain" -> app.configuration.getBoolean("cloudinary.cdn_subdomain").getOrElse(false),
-      "shorten" -> app.configuration.getBoolean("cloudinary.shorten").getOrElse(false)
-  ))
+  lazy val cloudinary = app.configuration.getString("cloudinary.url") match {
+    case Some(url) => 
+      new Cloudinary(url)
+    case None =>
+      new Cloudinary(Map(
+          "api_key" -> app.configuration.getString("cloudinary.api_key").getOrElse(throw new IllegalArgumentException("Configuration must include api_key")),
+          "api_secret" -> app.configuration.getString("cloudinary.api_secret").getOrElse(throw new IllegalArgumentException("Configuration must include api_secret")),
+          "cloud_name" -> app.configuration.getString("cloudinary.cloud_name").getOrElse(throw new IllegalArgumentException("Configuration must include cloud_name")),
+          "cname" -> app.configuration.getString("cloudinary.cname"),
+          "secure_distribution" -> app.configuration.getString("cloudinary.secure_distribution"),
+          "private_cdn" -> app.configuration.getBoolean("cloudinary.private_cdn").getOrElse(false),
+          "secure" -> app.configuration.getBoolean("cloudinary.secure").getOrElse(false),
+          "cdn_subdomain" -> app.configuration.getBoolean("cloudinary.cdn_subdomain").getOrElse(false),
+          "shorten" -> app.configuration.getBoolean("cloudinary.shorten").getOrElse(false)
+      ))
+  }
+  
   override lazy val enabled = {
     !app.configuration.getString("cloudinaryplugin").filter(_ == "disabled").isDefined
   }
