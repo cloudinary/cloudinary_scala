b2d51185f197ad6554edcbc40d221c1fbc5bf45b
Tal Lev-Ami <tal.levami@cloudinary.com>
2016-07-14 17:51:25 +0300

    Increment to version 0.9.9-SNAPSHOT - support filenameContains and includeSubfolders in resource listing for configured customers

--- project/Common.scala
+++ project/Common.scala
@@ -1,5 +1,5 @@
 object Common {
-  def version = "0.9.8-SNAPSHOT"  
+  def version = "0.9.9-SNAPSHOT"  
   def playVersion = System.getProperty("play.version", "2.4.2")
   def scalaVersion =  "2.11.5"
   def scalaVersions =  Seq("2.10.4", scalaVersion)
