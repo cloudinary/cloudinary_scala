a4cdf07e6cb9d563b5776fb32c1e51f2a3af16d6
Nitzan Jaitman <nitzan@cloudinary.com>
2020-11-10 17:52:57 +0200

    Drop support for scala 2.10.4

--- project/Common.scala
+++ project/Common.scala
@@ -2,5 +2,5 @@ object Common {
   def version = "1.2.2"
   def playVersion = System.getProperty("play.version", "2.6.6")
   def scalaVersion =  "2.12.8"
-  def scalaVersions =  Seq("2.10.4", "2.11.5", scalaVersion)
+  def scalaVersions =  Seq("2.11.5", scalaVersion)
 }
